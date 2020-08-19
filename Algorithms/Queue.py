f9e9a10f15e4f9e6cf817372c5c004e32f032a0f
Innovator Arjun <vsarjun412@gmail.com>
2020-08-20 03:10:40 +0530

    Update Queue

--- Algorithms/Queue.py
+++ Algorithms/Queue.py
@@ -1,35 +1,37 @@
-#FIFO- First In First out- Queue
+# FIFO- First In First out- Queue
 
 class Queue:
 
     def __init__(self):
-        self.queue=[]
+        self.queue = []
 
     # O(1) running time, to check if the queue is empty
     def isEmpty(self):
-        return self.queue==[]
+        return self.queue == []
 
-    #O(1), to insert the element in the queue
-    def enqueue(self,data):
+    # O(1), to insert the element in the queue
+    def enqueue(self, data):
         self.queue.append(data)
 
-    #0(N), when the first element is deleted the whole array to be shifted left. Hence we use linkedlist
+    # 0(N), when the first element is deleted the whole array to be shifted left. Hence we use linkedlist
 
     def dequeue(self):
-        if self.size()<1:
+        if self.size() < 1:
             return "No item to delete"
-        data=self.queue[0]
+        data = self.queue[0]
         self.queue.remove(data)
         return data
-    #O(1), time complexity
+
+    # O(1), time complexity
     def peek(self):
         return self.queue[0]
 
-    #O(1) constant running time
+    # O(1) constant running time
     def size(self):
         return len(self.queue)
 
-queue=Queue()
+
+queue = Queue()
 queue.enqueue(10)
 
 queue.enqueue(20)
@@ -42,4 +44,3 @@ print(queue.dequeue())
 print(queue.dequeue())
 
 print(queue.dequeue())
-
